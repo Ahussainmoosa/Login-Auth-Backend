6bd8656af93a62e2d5991edd8e8c100ffe317f39
hawra <hawra.yusuf02@outlook.com>
2025-11-10 09:20:43 +0300

    created schema for courses

--- models/user.js
+++ models/user.js
@@ -17,6 +17,22 @@ const userSchema = mongoose.Schema({
   },
 });
 
+const courseSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref:'User',
+  },  
+  enrolledStudents:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+});
+
+
+  
+
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     delete returnedObject.hashedPassword;
