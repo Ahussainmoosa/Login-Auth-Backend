1bbc007c1f81c900f1c3260f711234479b21a588
hawra <hawra.yusuf02@outlook.com>
2025-11-12 20:27:23 +0300

    edited schema

--- models/assignment.js
+++ models/assignment.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
 
+const submissionSchema = mongoose.Schema({
+  answer:{
+    type: String,
+    required: true,
+  },
+  submissionDate: Date,
+});
+
 const assignmentSchema = mongoose.Schema({
-  title: String,
-  content: String,
+  title: {
+    type: String,
+    required: true,
+  }, 
+  content:{
+    type: String,
+  }, 
   dueDate: Date,
   course:{
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Course'},
-    createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+    ref: 'Course'
     },
+
+    Submission: [submissionSchema],
+    
 });
 
 const Assignment = mongoose.model('Assignment', assignmentSchema);
