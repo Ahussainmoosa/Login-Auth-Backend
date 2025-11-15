3ce17b7e80636ed614a68aab0d87025468928eb1
hawra <hawra.yusuf02@outlook.com>
2025-11-15 13:35:31 +0300

    fixed admin functionality + added assignments routes in admin controller (rewritten)

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,6 +7,7 @@ const adminPerm = require("../middleware/is-admin.js");
 router.use(adminPerm);
 router.use(isSignedIn);
 
+//COURSES
 //create new course
 router.post('/new', async(req, res) => {
     try{
@@ -74,6 +75,82 @@ router.delete('/:courseId', async (req, res) => {
 
 });
 
+
+//ASSIGNMENTS
+// Create new assignment
+router.post('/new', async (req, res) => {
+    try {
+        const createdAssignment = await Assignment.create(req.body);
+        res.status(201).json(createdAssignment);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+});
+
+// Edit form
+router.get('/:assignmentId/edit', async (req, res) => {
+    try {
+        const assignment = await Assignment.findById(req.params.assignmentId);
+        if (!assignment) {
+            res.status(404);
+            throw new Error('Assignment not found');
+        }
+    } catch (err) {
+        res.status(500).json({ err: 'Something went wrong' });
+        console.log(err);
+    }
+});
+
+// Submit form
+router.post('/', async (req, res) => {
+    try {
+        const createdAssignment = await Assignment.create(req.body);
+        res.status(201).json(createdAssignment);
+    } catch (err) {
+        res.status(500).json({ err: 'Something went wrong' });
+        console.log(err);
+    }
+});
+
+// Update
+router.put('/:assignmentId', async (req, res) => {
+    try {
+        const updatedAssignment = await Assignment.findByIdAndUpdate(
+            req.params.assignmentId,
+            req.body,
+            { new: true }
+        );
+        if (!updatedAssignment) {
+            res.status(404);
+            throw new Error('Assignment not found');
+        }
+        res.status(200).json(updatedAssignment);
+    } catch (err) {
+        if (res.statusCode === 404) {
+            res.json({ err: 'Something went wrong' });
+            console.log(err);
+        } else {
+            res.status(500).json({ err: 'Something went wrong' });
+            console.log(err);
+        }
+    }
+});
+
+// Delete
+router.delete('/:assignmentId', async (req, res) => {
+    try {
+        const assignment = await Assignment.findByIdAndDelete(req.params.assignmentId);
+        if (!assignment) {
+            res.status(404);
+            throw new Error('Assignment not found');
+        }
+        res.status(204).json();
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+});
 
 
 module.exports = router;
--- controllers/assignments.js
+++ controllers/assignments.js
@@ -3,16 +3,6 @@ const User = require('../models/user.js'); // Fixed: assuming you meant User mod
 const express = require('express');
 const router = express.Router();
 
-// Create new assignment
-router.post('/new', async (req, res) => {
-    try {
-        const createdAssignment = await Assignment.create(req.body);
-        res.status(201).json(createdAssignment);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ err: 'Something went wrong' });
-    }
-});
 
 // Index of assignments
 router.get('/', async (req, res) => {
@@ -45,68 +35,6 @@ router.get('/:assignmentId', async (req, res) => {
     }
 });
 
-// Edit form
-router.get('/:assignmentId/edit', async (req, res) => {
-    try {
-        const assignment = await Assignment.findById(req.params.assignmentId);
-        if (!assignment) {
-            res.status(404);
-            throw new Error('Assignment not found');
-        }
-    } catch (err) {
-        res.status(500).json({ err: 'Something went wrong' });
-        console.log(err);
-    }
-});
 
-// Submit form
-router.post('/', async (req, res) => {
-    try {
-        const createdAssignment = await Assignment.create(req.body);
-        res.status(201).json(createdAssignment);
-    } catch (err) {
-        res.status(500).json({ err: 'Something went wrong' });
-        console.log(err);
-    }
-});
-
-// Update
-router.put('/:assignmentId', async (req, res) => {
-    try {
-        const updatedAssignment = await Assignment.findByIdAndUpdate(
-            req.params.assignmentId,
-            req.body,
-            { new: true }
-        );
-        if (!updatedAssignment) {
-            res.status(404);
-            throw new Error('Assignment not found');
-        }
-        res.status(200).json(updatedAssignment);
-    } catch (err) {
-        if (res.statusCode === 404) {
-            res.json({ err: 'Something went wrong' });
-            console.log(err);
-        } else {
-            res.status(500).json({ err: 'Something went wrong' });
-            console.log(err);
-        }
-    }
-});
-
-// Delete
-router.delete('/:assignmentId', async (req, res) => {
-    try {
-        const assignment = await Assignment.findByIdAndDelete(req.params.assignmentId);
-        if (!assignment) {
-            res.status(404);
-            throw new Error('Assignment not found');
-        }
-        res.status(204).json();
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ err: 'Something went wrong' });
-    }
-});
 
 module.exports = router;
