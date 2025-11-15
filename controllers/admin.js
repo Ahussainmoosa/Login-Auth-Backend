abcc2fe30c954bc96ead95b9d9dfd139482f594d
hawra <hawra.yusuf02@outlook.com>
2025-11-15 15:29:20 +0300

    removed admin course route from admin.js to course.js controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,73 +10,6 @@ router.use(isSignedIn);
 router.use(adminPerm);
 
 
-//COURSES
-//create new course
-router.post('/new', async(req, res) => {
-    try{
-        const createdCourse = await Course.create(req.body);
-        res.status(201).json(createdCourse);
-
-    } catch (err){
-        console.log(err);
-        res.status(500).json({err: 'Something went wrong'})
-    }
-});
-
-//edit form
-router.get('/:courseId/edit', async(req, res) => {
-    try{
-        const course = await Course.findById(req.params.courseId);
-        if (!course){
-            res.status(404);
-            throw new Error('Course not found');
-        }
-        res.status(200).json(course);
-    } catch (err){
-        res.status(500).json({err: 'Something went wrong'});
-        console.log(err);
-    }
-});
-
-//update
-router.put('/:courseId', async (req, res) => {
-    try{
-        const updatedCourse = await Course.findByIdAndUpdate(req.params.courseId, req.body, {
-            new: true,
-        });
-        if (!updatedCourse){
-            res.status(404);
-            throw new Error('Course not found');
-        }
-        res.status(200).json(updatedCourse);
-
-    } catch (err){
-        if(res.statusCode === 404){
-            res.json({err:'Something went wrong'})
-            console.log(err);
-        } else {
-            res.status(500).json({err:'Something went wrong'})
-            console.log(err);
-        }
-    }
-});
-
-//delete
-router.delete('/:courseId', async (req, res) => {
-    try{
-        const course = await Course.findByIdAndDelete(req.params.courseId);
-        if (!course){
-            res.status(404);
-            throw new Error('Course not found');
-        }
-        res.status(204).json;
-    } catch (err){
-        console.log(err);
-        res.status(500).json({err:'Something went wrong'})
-    }
-    
-
-});
 
 
 //ASSIGNMENTS
@@ -100,7 +33,7 @@ router.get('/:assignmentId/edit', async (req, res) => {
             throw new Error('Assignment not found');
         }
         res.status(200).json(assignment);
-        
+
     } catch (err) {
         res.status(500).json({ err: 'Something went wrong' });
         console.log(err);
--- controllers/courses.js
+++ controllers/courses.js
@@ -1,4 +1,6 @@
 const Course = require('../models/course.js');
+const isSignedIn = require("../middleware/is-signed-in.js");
+const adminPerm = require("../middleware/is-admin.js");
 const express = require('express');
 const router = express.Router();
 
@@ -15,6 +17,17 @@ router.get('/', async (req, res) => {
     }
 }); 
 
+//create new course
+router.post('/new', isSignedIn, adminPerm, async(req, res) => {
+    try{
+        const createdCourse = await Course.create(req.body);
+        res.status(201).json(createdCourse);
+
+    } catch (err){
+        console.log(err);
+        res.status(500).json({err: 'Something went wrong'})
+    }
+});
 
 //single course shown
 router.get('/:courseId', async (req, res) => {
@@ -38,5 +51,60 @@ router.get('/:courseId', async (req, res) => {
     }
 });
 
+
+//edit form
+router.get('/:courseId/edit', isSignedIn, adminPerm, async(req, res) => {
+    try{
+        const course = await Course.findById(req.params.courseId);
+        if (!course){
+            res.status(404);
+            throw new Error('Course not found');
+        }
+        res.status(200).json(course);
+    } catch (err){
+        res.status(500).json({err: 'Something went wrong'});
+        console.log(err);
+    }
+});
+
+//update
+router.put('/:courseId', isSignedIn, adminPerm, async (req, res) => {
+    try{
+        const updatedCourse = await Course.findByIdAndUpdate(req.params.courseId, req.body, {
+            new: true,
+        });
+        if (!updatedCourse){
+            res.status(404);
+            throw new Error('Course not found');
+        }
+        res.status(200).json(updatedCourse);
+
+    } catch (err){
+        if(res.statusCode === 404){
+            res.json({err:'Something went wrong'})
+            console.log(err);
+        } else {
+            res.status(500).json({err:'Something went wrong'})
+            console.log(err);
+        }
+    }
+});
+
+//delete
+router.delete('/:courseId', isSignedIn, adminPerm, async (req, res) => {
+    try{
+        const course = await Course.findByIdAndDelete(req.params.courseId);
+        if (!course){
+            res.status(404);
+            throw new Error('Course not found');
+        }
+        res.status(204).json;
+    } catch (err){
+        console.log(err);
+        res.status(500).json({err:'Something went wrong'})
+    }
+    
+
+});
 
 module.exports = router;
